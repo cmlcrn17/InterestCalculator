b94ba5971d1946f5a8c9d0bde44584e8f8185702
ceren <cerdem@geobilgi.com>
2019-04-23 21:30:23 +0300

    Hesaplama yapıldı. Test edilecek.

--- app/src/main/java/com/cemilecerenerdem/interestcalculator/MainActivity.java
+++ app/src/main/java/com/cemilecerenerdem/interestcalculator/MainActivity.java
@@ -1,6 +1,10 @@
 package com.cemilecerenerdem.interestcalculator;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,14 +12,20 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText totalMoney;
     EditText interestRate;
-    EditText numberOfRate;
+    EditText dailyvalue;
+    EditText mountlyvalue;
+    EditText yearlyvalue;
     RadioGroup radioGroup;
     RadioButton radioButton;
+
+    ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,29 +34,56 @@ public class MainActivity extends AppCompatActivity {
 
         totalMoney = (EditText) findViewById(R.id.totalMoney);
         interestRate = (EditText) findViewById(R.id.interestRate);
-        numberOfRate = (EditText) findViewById(R.id.numberOfDate);
+        mountlyvalue = (EditText) findViewById(R.id.monthly_value);
+        yearlyvalue = (EditText) findViewById(R.id.yearly_value);
+        dailyvalue = (EditText) findViewById(R.id.daily_value);
         radioGroup = (RadioGroup) findViewById(R.id.radioGroupID);
 
     }
 
-    public void Calculate(View view)
-    {
-        int resultCalculate = 0;
+    public void Calculate(View view) {
+        double resultCalculate = 0;
         int selectedTime = radioGroup.getCheckedRadioButtonId();
         radioButton = findViewById(selectedTime);
         String value = radioButton.getText().toString();
 
-        switch (value){
-            case "Daily":
-                //Günlük Faiz = (Anapara / 100) x (faiz oranı / 365) x gün sayısı
-                break;
-            case "Mountly":
-                //Aylık Faiz = (anapara / 100) x (faiz oranı / 12) x ay sayısı
-                break;
-            case "Yearly":
-                //Yıllık Faiz = (anapara / 100) x (faiz oranı) x yıl sayısı
-                break;
+        if (value != null){
+
+            int i_totalMoney = Integer.parseInt(totalMoney.getText().toString());
+            double i_interestRate = Double.parseDouble(interestRate.getText().toString());
+
+            switch (value) {
+                case "Daily":
+                    int i_dailyvalue = Integer.parseInt(dailyvalue.getText().toString());
+                    resultCalculate = (i_totalMoney / 100) * (i_interestRate / 365) * i_dailyvalue;
+                    break;
+
+                case "Monthly":
+                    int i_montlyvalue = Integer.parseInt(mountlyvalue.getText().toString());
+                    resultCalculate = (i_totalMoney / 100) * (i_interestRate / 12) * i_montlyvalue;
+                    break;
+
+                case "Yearly":
+                    int i_yearlyvalue = Integer.parseInt(yearlyvalue.getText().toString());
+                    resultCalculate = (i_totalMoney / 100) * (i_interestRate / 10) * i_yearlyvalue;
+                    break;
+            }
         }
 
+        if (resultCalculate > 0.0) {
+
+            String resultValue = String.valueOf(resultCalculate);
+
+            Dialog dialog = new Dialog(this);
+            dialog.setContentView(R.layout.dialog_result);
+            dialog.setTitle("Calculator Message");
+
+            TextView info = (TextView) dialog.findViewById(R.id.txt_view);
+            info.setText(resultValue.toString());
+            dialog.show();
+
+        } else {
+            Toast.makeText(this, "Error!", Toast.LENGTH_LONG);
+        }
     }
 }
